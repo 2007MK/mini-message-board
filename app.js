3ec45dd5756a84e5b5741ee6144f89f0b78034e5
Mehul <mehulk2007@gmail.com>
2026-03-23 14:22:24 +0000

    style: add stylesheet

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const path = require("path");
 const { indexRouter } = require("./routes/index");
 const newRouter = require("./routes/new");
 const app = express();
 
 // to parse the form data properly
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 // setup view engine
 app.set("view engine", "ejs");
